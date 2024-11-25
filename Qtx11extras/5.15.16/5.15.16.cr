c018aba9101f383ddde14afd1f8dc00513f54178
Fulgurance <zohran.londais@gmail.com>
2024-11-25 23:48:56 +0000

    Removed useless instructions

--- Qtx11extras/5.15.16/5.15.16.cr
+++ Qtx11extras/5.15.16/5.15.16.cr
@@ -24,17 +24,6 @@ class Target < ISM::Software
 
         makeSource( arguments:  "INSTALL_ROOT=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
                     path:       buildDirectoryPath)
-
-        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin")
-
-        if isGreatestVersion
-            directoryContent("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin/qt#{majorVersion}", matchHidden: true).each do |filePath|
-                fileName = filePath.lchop(filePath[0..filePath.rindex("/")])
-                makeLink(   target: "/usr/bin/qt#{majorVersion}/#{fileName}",
-                            path:   "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin/#{fileName}",
-                            type:   :symbolicLinkByOverwrite)
-            end
-        end
     end
 
     def install
