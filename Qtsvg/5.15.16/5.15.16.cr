475baf3decc32c0888db53b03e07ebdf48602088
Fulgurance <zohran.londais@gmail.com>
2024-11-25 16:56:55 +0000

    Added proper qt version

--- Qtsvg/5.15.16/5.15.16.cr
+++ Qtsvg/5.15.16/5.15.16.cr
@@ -8,7 +8,8 @@ class Target < ISM::Software
     def configure
         super
 
-        runQmakeCommand(arguments:  "-set QT_INSTALL_PREFIX /usr                            \
+        runQmakeCommand(arguments:  "set QT_VERSION #{version}                              \
+                                    -set QT_INSTALL_PREFIX /usr                             \
                                     -set QT_INSTALL_ARCHDATA /usr/lib64/qt#{majorVersion}   \
                                     -set QT_INSTALL_DATA /usr/share/qt#{majorVersion}       \
                                     -set QT_INSTALL_DOCS /usr/share/doc/qt#{majorVersion}   \
