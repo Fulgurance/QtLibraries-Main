d956e7e56b376fbd08deb23802db9bc0bdb86cfb
Fulgurance <zohran.londais@gmail.com>
2024-11-25 13:14:48 +0000

    Added experimental qtbase installer

--- Qtbase/5.15.16/5.15.16.cr
+++ Qtbase/5.15.16/5.15.16.cr
@@ -1,43 +1,30 @@
 class Target < ISM::Software
     
-    def prepare
-        @buildDirectory = true
-        super
-    end
-
     def configure
         super
 
-        runCmakeCommand(arguments:      "-DCMAKE_INSTALL_PREFIX=/usr                                        \
-                                        -DINSTALL_ARCHDATADIR=/usr/lib/qt#{majorVersion}                    \
-                                        -DINSTALL_BINDIR=/usr/bin/qt#{majorVersion}                         \
-                                        -DINSTALL_PLUGINSDIR=/usr/lib/qt#{majorVersion}/plugin              \
-                                        -DINSTALL_INCLUDEDIR=/usr/include/qt#{majorVersion}                 \
-                                        -DINSTALL_DATADIR=/usr/share/qt#{majorVersion}                      \
-                                        -DINSTALL_DOCDIR=/usr/share/doc/qt#{majorVersion}                   \
-                                        -DINSTALL_TRANSLATIONSDIR=/usr/share/qt#{majorVersion}/translations \
-                                        -DINSTALL_SYSCONFDIR=/etc/xdg                                       \
-                                        -DQT_UNITY_BUILD=ON                                                 \
-                                        -DQT_FEATURE_relocatable=OFF                                        \
-                                        -DQT_FEATURE_androiddeployqt=OFF                                    \
-                                        -DQT_BUILD_EXAMPLES=OFF                                             \
-                                        -DQT_BUILD_DOCS=OFF                                                 \
-                                        -DFEATURE_rpath=OFF                                                 \
-                                        -DFEATURE_dbus=#{option("Dbus") ? "ON" : "OFF"}                     \
-                                        -DFEATURE_dbus_linked=#{option("Dbus") ? "ON" : "OFF"}              \
-                                        -DFEATURE_openssl=#{option("Openssl") ? "ON" : "OFF"}               \
-                                        -DFEATURE_openssl_linked=#{option("Openssl") ? "ON" : "OFF"}        \
-                                        -DFEATURE_system_freetype=ON                                        \
-                                        -DFEATURE_harfbuzz=#{option("Harfbuzz") ? "ON" : "OFF"}             \
-                                        -DFEATURE_system_harfbuzz=#{option("Harfbuzz") ? "ON" : "OFF"}      \
-                                        -DFEATURE_sql_sqlite=#{option("Sqlite") ? "ON" : "OFF"}             \
-                                        -DFEATURE_system_sqlite=#{option("Sqlite") ? "ON" : "OFF"}          \
-                                        -DFEATURE_xcb=#{option("Xcb") ? "ON" : "OFF"}                       \
-                                        -DFEATURE_cups=#{option("Cups") ? "ON" : "OFF"}                     \
-                                        -DFEATURE_system_zlib=ON                                            \
-                                        -DFEATURE_system_pcre2=ON                                           \
-                                        ..",
-                        path:           buildDirectoryPath)
+        configureSource(arguments:  "--prefix=/usr                                              \
+                                    --archdatadir=/usr/lib/qt#{majorVersion}                    \
+                                    --bindir=/usr/bin/qt#{majorVersion}                         \
+                                    --plugindir=/usr/lib/qt#{majorVersion}/plugins              \
+                                    --importdir=/usr/lib/qt#{majorVersion}/imports              \
+                                    --headerdir=/usr/include/qt#{majorVersion}                  \
+                                    --datadir=/usr/share/qt#{majorVersion}                      \
+                                    --docdir=/usr/share/doc/qt#{majorVersion}                   \
+                                    --translationdir=/usr/share/qt#{majorVersion}/translations  \
+                                    --sysconfdir=/etc/xdg                           \
+                                    --confirm-license                               \
+                                    --opensource                                    \
+                                    #{option("Dbus") ? "--dbus-linked" : ""}        \
+                                    #{option("Openssl") ? "--openssl-linked" : ""}  \
+                                    #{option("Harfbuzz") ? "-system-harfbuzz" : ""} \
+                                    #{option("Sqlite") ? "-system-sqlite" : ""}     \
+                                    #{option("Xcb") ? "-xcb" : ""}                  \
+                                    #{option("Cups") ? "-cups" : ""}                \
+                                    --nomake=examples                               \
+                                    --no-rpath                                      \
+                                    --syslog",
+                        path:       buildDirectoryPath)
     end
     
     def build
