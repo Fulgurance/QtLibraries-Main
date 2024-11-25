67a51b9d55cd4ff58ce9378eaf30822e5ee2231c
Fulgurance <zohran.londais@gmail.com>
2024-11-25 16:11:56 +0000

    Added qtsvg

---
+++ Qtsvg/5.15.16/5.15.16.cr
@@ -0,0 +1,65 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        prefix = "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr"
+
+        configureSource(arguments:  "--prefix=#{prefix}                                              \
+                                    --archdatadir=#{prefix}/lib/qt#{majorVersion}                    \
+                                    --bindir=#{prefix}/bin/qt#{majorVersion}                         \
+                                    --plugindir=#{prefix}/lib/qt#{majorVersion}/plugins              \
+                                    --importdir=#{prefix}/lib/qt#{majorVersion}/imports              \
+                                    --headerdir=#{prefix}/include/qt#{majorVersion}                  \
+                                    --datadir=#{prefix}/share/qt#{majorVersion}                      \
+                                    --docdir=#{prefix}/share/doc/qt#{majorVersion}                   \
+                                    --translationdir=#{prefix}/share/qt#{majorVersion}/translations  \
+                                    --sysconfdir=/etc/xdg                           \
+                                    --confirm-license                               \
+                                    --opensource                                    \
+                                    #{option("Dbus") ? "--dbus-linked" : ""}        \
+                                    #{option("Openssl") ? "--openssl-linked" : ""}  \
+                                    #{option("Harfbuzz") ? "-system-harfbuzz" : ""} \
+                                    #{option("Sqlite") ? "-system-sqlite" : ""}     \
+                                    #{option("Xcb") ? "-xcb" : ""}                  \
+                                    #{option("Cups") ? "-cups" : ""}                \
+                                    --nomake=examples                               \
+                                    --no-rpath                                      \
+                                    --syslog",
+                        path:       buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "install",
+                    path:       buildDirectoryPath)
+
+        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin")
+
+        if isGreatestVersion
+            directoryContent("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin/qt#{majorVersion}", matchHidden: true).each do |filePath|
+
+                fileName = filePath.lchop(filePath[0..filePath.rindex("/")])
+
+                makeLink(   target: "/usr/bin/qt#{majorVersion}/#{fileName}",
+                            path:   "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin/#{fileName}",
+                            type:   :symbolicLinkByOverwrite)
+
+            end
+        end
+    end
+
+    def install
+        super
+
+        runLdconfigCommand
+    end
+
+end
---
+++ Qtsvg/6.7.3/6.7.3.cr
@@ -0,0 +1,78 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        prefix = "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr"
+
+        runCmakeCommand(arguments:      "-DCMAKE_INSTALL_PREFIX=#{prefix}                                        \
+                                        -DINSTALL_ARCHDATADIR=#{prefix}/lib/qt#{majorVersion}                    \
+                                        -DINSTALL_BINDIR=#{prefix}/bin/qt#{majorVersion}                         \
+                                        -DINSTALL_PLUGINSDIR=#{prefix}/lib/qt#{majorVersion}/plugin              \
+                                        -DINSTALL_INCLUDEDIR=#{prefix}/include/qt#{majorVersion}                 \
+                                        -DINSTALL_DATADIR=#{prefix}/share/qt#{majorVersion}                      \
+                                        -DINSTALL_DOCDIR=#{prefix}/share/doc/qt#{majorVersion}                   \
+                                        -DINSTALL_TRANSLATIONSDIR=#{prefix}/share/qt#{majorVersion}/translations \
+                                        -DINSTALL_SYSCONFDIR=/etc/xdg                                       \
+                                        -DQT_UNITY_BUILD=ON                                                 \
+                                        -DQT_FEATURE_relocatable=OFF                                        \
+                                        -DQT_FEATURE_androiddeployqt=OFF                                    \
+                                        -DQT_BUILD_EXAMPLES=OFF                                             \
+                                        -DQT_BUILD_DOCS=OFF                                                 \
+                                        -DFEATURE_rpath=OFF                                                 \
+                                        -DFEATURE_dbus=#{option("Dbus") ? "ON" : "OFF"}                     \
+                                        -DFEATURE_dbus_linked=#{option("Dbus") ? "ON" : "OFF"}              \
+                                        -DFEATURE_openssl=#{option("Openssl") ? "ON" : "OFF"}               \
+                                        -DFEATURE_openssl_linked=#{option("Openssl") ? "ON" : "OFF"}        \
+                                        -DFEATURE_system_freetype=ON                                        \
+                                        -DFEATURE_harfbuzz=#{option("Harfbuzz") ? "ON" : "OFF"}             \
+                                        -DFEATURE_system_harfbuzz=#{option("Harfbuzz") ? "ON" : "OFF"}      \
+                                        -DFEATURE_sql_sqlite=#{option("Sqlite") ? "ON" : "OFF"}             \
+                                        -DFEATURE_system_sqlite=#{option("Sqlite") ? "ON" : "OFF"}          \
+                                        -DFEATURE_xcb=#{option("Xcb") ? "ON" : "OFF"}                       \
+                                        -DFEATURE_cups=#{option("Cups") ? "ON" : "OFF"}                     \
+                                        -DFEATURE_system_zlib=ON                                            \
+                                        -DFEATURE_system_pcre2=ON                                           \
+                                        ..",
+                        path:           buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "install",
+                    path:       buildDirectoryPath)
+
+        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin")
+
+        if isGreatestVersion
+            directoryContent("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin/qt#{majorVersion}", matchHidden: true).each do |filePath|
+
+                fileName = filePath.lchop(filePath[0..filePath.rindex("/")])
+
+                makeLink(   target: "/usr/bin/qt#{majorVersion}/#{fileName}",
+                            path:   "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin/#{fileName}",
+                            type:   :symbolicLinkByOverwrite)
+
+            end
+        end
+    end
+
+    def install
+        super
+
+        runLdconfigCommand
+    end
+
+end
