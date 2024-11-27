5c158aaa570bbd2c1e477962637a609e09236e68
Fulgurance <zohran.londais@gmail.com>
2024-11-27 13:52:06 +0000

    Added qtspeech

--- Qtspeech/6.7.3/6.7.3.cr
+++ Qtspeech/6.7.3/6.7.3.cr
@@ -1,5 +1,5 @@
 class Target < ISM::Software
-
+    
     def prepare
         @buildDirectory = true
         super
@@ -8,21 +8,21 @@ class Target < ISM::Software
     def configure
         super
 
-        runQmakeCommand(arguments:  "..",
-                        path:       buildDirectoryPath,
+        runCmakeCommand(arguments:      "..",
+                        path:           buildDirectoryPath,
                         environment:    {"PATH" => "/usr/bin/qt#{majorVersion}:$PATH"})
     end
-    
+
     def build
         super
 
         makeSource(path: buildDirectoryPath)
     end
-    
+
     def prepareInstallation
         super
 
-        makeSource( arguments:  "INSTALL_ROOT=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
                     path:       buildDirectoryPath)
 
         makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/bin")
