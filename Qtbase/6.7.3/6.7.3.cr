c932b3c71e6aecb77ca63ebf0c1892469c9082ac
Fulgurance <zohran.londais@gmail.com>
2025-01-26 19:48:54 +0000

    Updated configuration process

--- Qtbase/6.7.3/6.7.3.cr
+++ Qtbase/6.7.3/6.7.3.cr
@@ -34,7 +34,7 @@ class Target < ISM::Software
                                         -DFEATURE_system_harfbuzz=#{option("Harfbuzz") ? "ON" : "OFF"}      \
                                         -DFEATURE_sql_sqlite=#{option("Sqlite") ? "ON" : "OFF"}             \
                                         -DFEATURE_system_sqlite=#{option("Sqlite") ? "ON" : "OFF"}          \
-                                        -DFEATURE_xcb=#{option("Xcb") ? "ON" : "OFF"}                       \
+                                        -DFEATURE_xkbcommon_x11=#{option("Libxkbcommon") ? "ON" : "OFF"}    \
                                         -DFEATURE_cups=#{option("Cups") ? "ON" : "OFF"}                     \
                                         -DFEATURE_system_zlib=ON                                            \
                                         -DFEATURE_system_pcre2=#{option("Pcre2") ? "ON" : "OFF"}",
