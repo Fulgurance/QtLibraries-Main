fdf7c15e74e0f3bdd63c26e166d84857462e564b
Fulgurance <zohran.londais@gmail.com>
2025-01-27 16:34:15 +0000

    Added experimental patch

--- Qtbase/6.7.3/6.7.3.cr
+++ Qtbase/6.7.3/6.7.3.cr
@@ -34,7 +34,8 @@ class Target < ISM::Software
                                         -DFEATURE_system_harfbuzz=#{option("Harfbuzz") ? "ON" : "OFF"}      \
                                         -DFEATURE_sql_sqlite=#{option("Sqlite") ? "ON" : "OFF"}             \
                                         -DFEATURE_system_sqlite=#{option("Sqlite") ? "ON" : "OFF"}          \
-                                        -DFEATURE_xcb=#{option("Xcb") ? "ON" : "OFF"}         \
+                                        -DFEATURE_xcb=#{option("Xcb") ? "ON" : "OFF"}                       \
+                                        -DFEATURE_xkbcommon_x11=#{option("Xcb") ? "ON" : "OFF"}             \
                                         -DFEATURE_cups=#{option("Cups") ? "ON" : "OFF"}                     \
                                         -DFEATURE_system_zlib=ON                                            \
                                         -DFEATURE_system_pcre2=#{option("Pcre2") ? "ON" : "OFF"}",
